f9c6ebe4d56d4ddf011a8fd2af289fe78278df03
Edvan <eadelima@outlook.com>
2019-10-08 12:33:15 -0300

    add repo

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return "It works!";
+    }
+
+    public function create()
+    {
+        return "It works!";
+    }
+
+    public function store(Request $request)
+    {
+        return "It works!";
+    }
+
+    public function show($id)
+    {
+        return "It works!";
+    }
+
+    public function edit($id)
+    {
+        return "It works!";
+    }
+
+    public function update(Request $request, $id)
+    {
+        return "It works!";
+    }
+
+    public function destroy($id)
+    {
+        return "It works!";
+    }
+}
